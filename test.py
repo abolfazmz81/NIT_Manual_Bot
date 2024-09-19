43c48700e99058d36e15d0314d7db80d40a77cf9
abolfazmz81 <abolfazmz81@gmail.com>
2024-09-19 15:46:03 +0330

    faet: Add message handler

--- test.py
+++ test.py
@@ -11,6 +11,13 @@ async def start(update: Update, context: CallbackContext) -> None:
     # wlcoming the user
     await update.message.reply_text(f'hi {wade}!, welcome to the show')
 
+async def mem(update: Update, context: CallbackContext) -> None:
+    print(update.message.text)
+        mes = input(f"answer the message from {update.message.from_user.username}:")
+        if mes != "":
+            await update.message.reply_text(mes, reply_to_message_id=update.message.message_id)
+    print("done")
+
 
 def main():
     Token = config.telegram_token

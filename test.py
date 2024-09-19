43a2519de93308e2277cb53a939e573fdb134b92
abolfazmz81 <abolfazmz81@gmail.com>
2024-09-19 15:49:05 +0330

    feat: Add ChatGPT Api

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler
+from langchain_openai import ChatOpenAI
 import config
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", base_url="https://api.avalai.ir/v1",
+                 api_key=config.api_token)
+
+gptc = True
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -11,8 +17,21 @@ async def start(update: Update, context: CallbackContext) -> None:
     # wlcoming the user
     await update.message.reply_text(f'hi {wade}!, welcome to the show')
 
+async def gpt(update: Update, context: CallbackContext) -> None:
+    global gptc
+    gptc = True
+    await update.message.reply_text("Please send the message you want to ask ChatGPT.")
+
+
 async def mem(update: Update, context: CallbackContext) -> None:
+    global gptc
     print(update.message.text)
+    if gptc:
+        query = update.message.text
+        response = llm.invoke(query)
+        await update.message.reply_text(response.content.__str__())
+        gptc = False
+    else:
         mes = input(f"answer the message from {update.message.from_user.username}:")
         if mes != "":
             await update.message.reply_text(mes, reply_to_message_id=update.message.message_id)
@@ -25,6 +44,8 @@ def main():
     application = Application.builder().token(Token).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("gpt", gpt))
+    application.add_handler(MessageHandler(None, mem))
     application.run_polling()
 
 
